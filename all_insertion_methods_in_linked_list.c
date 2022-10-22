64623c2ab40e479f088d330a334116fb4a9ecc43
Peeyush <peeyushkumarpks_cse20@its.edu.in>
2022-10-22 13:31:40 +0530

    modification_in_insertion_method_in_linked_list.c

--- all_insertion_methods_in_linked_list.c
+++ all_insertion_methods_in_linked_list.c
@@ -14,16 +14,16 @@ struct Node *next;
 an int, inserts a new node on the front of the list. */
 void push(struct Node** head_ref, int new_data)
 {
-	/* 1. allocate node */
+	//1. allocate node 
 	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
 
-	/* 2. put in the data */
+	// 2. put in the data
 	new_node->data = new_data;
 
-	/* 3. Make next of new node as head */
+	// 3. Make next of new node as head 
 	new_node->next = (*head_ref);
 
-	/* 4. move the head to point to the new node */
+	//4. move the head to point to the new node 
 	(*head_ref) = new_node;
 }
 
@@ -31,23 +31,23 @@ void push(struct Node** head_ref, int new_data)
 prev_node */
 void insertAfter(struct Node* prev_node, int new_data)
 {
-	/*1. check if the given prev_node is NULL */
+	//1. check if the given prev_node is NULL
 	if (prev_node == NULL)
 	{
 	printf("the given previous node cannot be NULL");
 	return;
 	}
 
-	/* 2. allocate new node */
+	// 2. allocate new node
 	struct Node* new_node =(struct Node*) malloc(sizeof(struct Node));
 
-	/* 3. put in the data */
+	// 3. put in the data 
 	new_node->data = new_data;
 
-	/* 4. Make next of new node as next of prev_node */
+	// 4. Make next of new node as next of prev_node 
 	new_node->next = prev_node->next;
 
-	/* 5. move the next of prev_node as new_node */
+	//5. move the next of prev_node as new_node 
 	prev_node->next = new_node;
 }
 
@@ -55,30 +55,30 @@ void insertAfter(struct Node* prev_node, int new_data)
 of a list and an int, appends a new node at the end */
 void append(struct Node** head_ref, int new_data)
 {
-	/* 1. allocate node */
+	// 1. allocate node
 	struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
 
 	struct Node *last = *head_ref; /* used in step 5*/
 
-	/* 2. put in the data */
+	//2. put in the data 
 	new_node->data = new_data;
 
 	/* 3. This new node is going to be the last node, so make next of
 		it as NULL*/
 	new_node->next = NULL;
 
-	/* 4. If the Linked List is empty, then make the new node as head */
+	// 4. If the Linked List is empty, then make the new node as head
 	if (*head_ref == NULL)
 	{
 	*head_ref = new_node;
 	return;
 	}
 
-	/* 5. Else traverse till the last node */
+	//5. Else traverse till the last node 
 	while (last->next != NULL)
 		last = last->next;
 
-	/* 6. Change the next of last node */
+	// 6. Change the next of last node 
 	last->next = new_node;
 	return;
 }
@@ -93,10 +93,10 @@ while (node != NULL)
 }
 }
 
-/* Driver program to test above functions*/
+// Driver program to test above functions
 int main()
 {
-/* Start with the empty list */
+// Start with the empty list
 struct Node* head = NULL;
 
 // Insert 6. So linked list becomes 6->NULL
